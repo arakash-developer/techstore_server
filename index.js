6fa8975dc9141b300d6f3551ac7d68bdff00904b
akash100a <arakash968786@gmail.com>
2024-12-21 18:19:51 +0600

    up

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors  = require('cors');  
 app.use(express.json());
+app.use(cors());
 let ProductRoutes = require('./routes/product');
 
 
